a0c54ea44ecce5b2fc2d96426b62db89d76e1d88
Asmit patel <asmitpatel.er27@gmail.com>
2024-09-07 23:49:43 +0530

    work

---
+++ 04_whileloop/Q_01.cpp
@@ -0,0 +1,14 @@
+//Print number 1 to N .
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n) {
+    cout<<i<<" ";
+    i=i+1;
+
+    }
+
+}
