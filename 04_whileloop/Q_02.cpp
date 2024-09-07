1114aab3881c8a231affc148ad6e48d19f53f3b1
Asmit patel <asmitpatel.er27@gmail.com>
2024-09-08 01:11:50 +0530

    question

---
+++ 04_whileloop/Q_02.cpp
@@ -0,0 +1,19 @@
+// sum of number 1 to N
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    int i=1;
+    int sum=0;
+    
+    while (i<=n) {
+        sum=sum+i;
+        i=i+1;
+        
+    }
+    cout<<"value of sum is "<<sum<<endl;
+
+    return 0;
+}
