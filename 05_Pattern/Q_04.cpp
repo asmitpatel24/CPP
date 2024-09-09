e96d3e0dfea717da8891bb77bd5fc00952d92d88
Asmit patel <asmitpatel.er27@gmail.com>
2024-09-09 23:23:45 +0530

    pattern

---
+++ 05_Pattern/Q_04.cpp
@@ -0,0 +1,25 @@
+/* Q- print pattern 4321
+                    4321
+                    4321
+                    4321*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int i=1;
+
+    while (i<=n) {
+
+        int j=1;
+
+        while (j<=n) {
+            cout<< n-j+1 ;  //n-j+1 is a formula for opposite.. 
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+return 0;
+}
