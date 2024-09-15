aca86c6569aad7fe67f0896d625fd72668ec26fa
Asmit patel <asmitpatel.er27@gmail.com>
2024-09-15 10:42:01 +0530

    typecasting

--- 01_variable-and-datatype/typcstng.cpp
+++ 01_variable-and-datatype/typcstng.cpp
@@ -1,3 +1,4 @@
+//Type casting, or type conversion, is a fundamental concept in programming that involves converting one data type into another. This process is crucial for ensuring compatibility and flexibility within a program.
 #include<iostream>
 using namespace std;
 int main() {
